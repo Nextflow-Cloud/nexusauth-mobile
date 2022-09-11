b72c86a36477c61d4b3b83e4a10b64e85e4a6f98
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-09-11 11:59:44 -0500

    feat: start from scratch

--- NexusAuth/App.xaml.cs
+++ NexusAuth/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using NexusAuth.Services;
-using NexusAuth.Views;
-using System;
+﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,13 +6,11 @@ namespace NexusAuth
 {
     public partial class App : Application
     {
-
         public App()
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
