e60dd491418582e82dc954bdb41c7092fab7f6a3
Infiniwave <61062461+infiniwave@users.noreply.github.com>
2022-09-11 10:59:22 -0500

    feat: initial commit

---
+++ NexusAuth/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using NexusAuth.ViewModels;
+using NexusAuth.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace NexusAuth
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ NexusAuth/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NexusAuth.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NexusAuth.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
